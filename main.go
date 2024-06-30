023d3410199860811f978e9a83c43e87d1d35cfe
Brian Zhou <zhouyunping@gmail.com>
2024-06-30 00:45:44 -0500

    testing mongo

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var collection *mongo.Collection
+var ctx = context.TODO()
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Home page</h1>")
@@ -13,10 +25,54 @@ func teamsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Teams!!</h1>")
 }
 
-func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/teams", teamsHandler)
+// func main() {
+// 	http.HandleFunc("/", homeHandler)
+// 	http.HandleFunc("/teams", teamsHandler)
 
-	fmt.Println("Starting the server on port 3000...")
-	http.ListenAndServe(":3000", nil)
+// 	fmt.Println("Starting the server on port 3000...")
+// 	http.ListenAndServe(":3000", nil)
+// }
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
+			"See: " +
+			"www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().
+		ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	coll := client.Database("sample_mflix").Collection("movies")
+	title := "Back to the Future"
+
+	var result bson.M
+	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).
+		Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the title %s\n", title)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
 }
