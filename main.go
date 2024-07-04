550b6ceac86071b36168c628b0b75beaa9175efc
Brian Zhou <zhouyunping@gmail.com>
2024-07-04 01:59:08 -0500

    added routes to handle tournament start

--- main.go
+++ main.go
@@ -16,14 +16,18 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hi</h1>")
+var coll *mongo.Collection
+var err error
+
+func tournamentHandler(w http.ResponseWriter, r *http.Request) {
 	filter := bson.D{{Key: "status", Value: "Active"}}
 	var result models.Tournament
 	err = coll.FindOne(context.TODO(), filter).Decode(&result)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-
+			w.WriteHeader(http.StatusNotFound)
+			errorResponse := map[string]string{"error": "No available tournaments found"}
+			json.NewEncoder(w).Encode(errorResponse)
 			return
 		}
 		panic(err)
@@ -34,25 +38,30 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	w.Write(output)
-
-	fmt.Printf("%s\n", output)
-	//fmt.Fprintf(w, "<h1>Welcome to %v!</h1>", result.Name)
 }
 
-func teamsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Teams</h1>")
+func tournamentStartHandler(w http.ResponseWriter, r *http.Request) {
+	filter := bson.D{{Key: "status", Value: "Registering"}}
+	update := bson.D{{"$set", bson.D{{"status", "Active"}}}}
+
+	_, err := coll.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			w.WriteHeader(http.StatusNotFound)
+			errorResponse := map[string]string{"error": "No available tournaments to start"}
+			json.NewEncoder(w).Encode(errorResponse)
+			return
+		}
+		panic(err)
+	}
 }
-
-var coll *mongo.Collection
-var err error
-
 func main() {
 	// Load .env file
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
 
-	// Making database connection
+	// Connect to database
 	uri := os.Getenv("DATABASE_URL")
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
@@ -67,9 +76,12 @@ func main() {
 
 	coll = client.Database("tournament_scoring").Collection("tournaments")
 
-	http.HandleFunc("/api", homeHandler)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /api/tournament", tournamentHandler)
+	mux.HandleFunc("POST /api/tournament/start", tournamentStartHandler)
 	fmt.Println("Starting server on port 8000")
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", mux)
 	// 	newTournament := models.Tournament{Name: "E4E 2024", Date: time.Now(), Status: "Active"}
 
 	// 	result, err := coll.InsertOne(context.TODO(), newTournament)
