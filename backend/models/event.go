19c9a2f210a36cd4de4404ecb9d01dff2d4705b1
Brian Zhou <zhouyunping@gmail.com>
2024-07-22 21:44:20 -0500

    add events and event times

--- backend/models/event.go
+++ backend/models/event.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"context"
-	"time"
 
 	"github.com/CrossoversForCures/Tournament-Scoring/backend/configs"
 	"go.mongodb.org/mongo-driver/bson"
@@ -13,7 +12,7 @@ type Event struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 	Name       string             `bson:"name,omitempty" json:"name,omitempty"`
 	Slug       string             `bson:"slug,omitempty" json:"slug,omitempty"`
-	Time       time.Time          `bson:"time,omitempty" json:"time,omitempty"`
+	Time       string             `bson:"time,omitempty" json:"time,omitempty"`
 	Status     int                `bson:"status,omitempty" json:"status,omitempty"` //0: In registration | 1: Playing pools | 2: Playing bracket | 3: Completed
 	PoolRounds int                `bson:"poolRounds,omitempty" json:"poolRounds,omitempty"`
 }
@@ -56,16 +55,9 @@ func InitiateEvents() {
 	}
 
 	newEvents := []interface{}{
-		Event{Name: "3rd/4th Boys", Slug: "3rd-4th-boys"},
-		Event{Name: "5th/6th Boys", Slug: "5th-6th-boys"},
-		Event{Name: "7th/8th Boys", Slug: "7th-8th-boys"},
-		Event{Name: "9th/10th Boys", Slug: "9th-10th-boys"},
-		Event{Name: "11th/12th Boys", Slug: "11th-12th-boys"},
-		Event{Name: "3rd/4th Girls", Slug: "3rd-4th-girls"},
-		Event{Name: "5th/6th Girls", Slug: "5th-6th-girls"},
-		Event{Name: "7th/8th Girls", Slug: "7th-8th-girls"},
-		Event{Name: "9th/12th Girls", Slug: "9th-12th-girls"},
-		Event{Name: "College Co-ed", Slug: "college-coed"},
+		Event{Name: "5th/6th Boys", Slug: "5th-6th-boys", Time: "3:00PM"},
+		Event{Name: "7th/8th Boys", Slug: "7th-8th-boys", Time: "11:30AM"},
+		Event{Name: "5th/6th Girls", Slug: "5th-6th-girls", Time: "9:00AM"},
 	}
 
 	_, err = configs.EventsCollection.InsertMany(context.TODO(), newEvents)
