55a618965800ffd16f678b4fad77803afb9d6f55
Brian Zhou <zhouyunping@gmail.com>
2024-07-26 22:18:17 -0500

    cookie fix

--- frontend/src/routes/home/+page.server.ts
+++ frontend/src/routes/home/+page.server.ts
@@ -10,7 +10,7 @@ export const actions = {
         const password = data.get('password');
 
         if (username && password && username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
-            cookies.set('session', 'admin', { path: '/', httpOnly: true, sameSite: 'strict', maxAge: 60 * 60 * 24 * 7 });
+            cookies.set('session', 'admin', { path: '/', httpOnly: false, sameSite: 'strict', maxAge: 60 * 60 * 24 * 7 });
             throw redirect(303, '/');
         }
         return fail(400, { incorrect: true });
